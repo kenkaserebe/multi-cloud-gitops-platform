0cc2114e9b83b15188367e82e88b4c9df2d134f0
kazaraal <araalkaz@gmail.com>
2026-04-04 07:06:01 +0100

    Arranged files for better visual

--- environments/aws/main.tf
+++ environments/aws/main.tf
@@ -35,10 +35,11 @@ resource "aws_ecr_repository" "app_repo" {
   }
 }
 
+
+# Output ecr repository url
 output "ecr_repository_url" {
   value = aws_ecr_repository.app_repo.repository_url
 }
-
 
 # Data sources to get authentication info for the EKS cluster
 data "aws_eks_cluster" "this" {
@@ -57,25 +58,3 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
-
-
-
-
-
-
-
-
-
-
-# Output ecr repository url
-output "ecr_repository_url" {
-  value = aws_ecr_repository.app_repo.repository_url
-}
-
-
-# Add the EBS CSI Driver Add-on
-# resource "aws_eks_addon" "ebs_csi" {
-#   cluster_name = module.eks_cluster.cluster_name
-#   addon_name   = "aws-ebs-csi-driver"
-#   depends_on   = [module.eks_cluster] # Ensure cluster exists first
-# }
